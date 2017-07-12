65ab13f713b9973c366f33f5c2dd7439a4754ba1
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-07-12 09:08:09 +0200

    New: doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+﻿// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// package anytype provides templates and it's subdirectories hold refining templates, and resulting go code
+package anytype
