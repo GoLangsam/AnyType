b9d4cdfa1434ed6e12b4f921685447db6d88e91b
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-07-15 16:27:39 +0200

    new: dot.go
    just a noop - avoids 'no buildable Go source files' warning of `go get`

---
+++ chan/dot.go
@@ -0,0 +1,12 @@
+﻿// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// package tube provides base templates for various flavours of tubes
+package tube
+
+import (
+	_	"github.com/GoLangsam/AnyType"
+)
+
+const dot = `.`
