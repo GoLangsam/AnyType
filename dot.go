76cd9e5fff6bff90290b706a2b14ccd80198a1ac
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-07-20 10:44:33 +0200

    make `golint` more happy

--- dot.go
+++ dot.go
@@ -1,8 +1,8 @@
-﻿// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Copyright 2017 Andreas Pannewitz. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// package anytype provides templates and it's subdirectories hold refining templates, and resulting go code
+// Package anytype provides templates and it's subdirectories hold refining templates, and resulting go code
 package anytype
 
-const dot = `.`
+const dot = `.` // just a noop - avoids 'no buildable Go source files' warning of `go get`
