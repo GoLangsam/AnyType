c260ade9822a98ba4f182c0545e0e907e03e01b4
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-07-20 10:43:40 +0200

    make `golint` more happy

--- chan/dot.go
+++ chan/dot.go
@@ -1,12 +1,12 @@
-﻿// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Copyright 2017 Andreas Pannewitz. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// package tube provides base templates for various flavours of tubes
+// Package tube provides base templates for various flavours of tubes
 package tube
 
 import (
-	_	"github.com/GoLangsam/AnyType"
+	_	"github.com/GoLangsam/AnyType" // indicate need of higher level templates
 )
 
-const dot = `.`
+const dot = `.` // just a noop - avoids 'no buildable Go source files' warning of `go get`
