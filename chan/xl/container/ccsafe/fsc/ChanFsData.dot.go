07447476995a95fd4df2a98daf09b5478221fcb0
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-08-17 18:24:28 +0200

    add: container

---
+++ chan/xl/container/ccsafe/fsc/ChanFsData.dot.go
@@ -0,0 +1,16 @@
+// Copyright 2017 Andreas Pannewitz. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fsc
+
+// This file was generated with dotgo
+// DO NOT EDIT - Improve the pattern!
+
+import (
+	"github.com/golangsam/container/ccsafe/fs"
+)
+
+var _ *fs.FsData // silence import
+
+// No functionality yet associated with this source file.
