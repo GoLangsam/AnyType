63421ce6f9d51c339d5058ea0727a5f3581b60c1
Andreas Pannewitz <GoLangsam.APA@a-bc.net>
2017-08-22 05:00:51 +0200

    refined: support core, cleanup SendProxy

--- chan/sss/duplicate.cmd
+++ chan/sss/duplicate.cmd
@@ -1,8 +1,40 @@
-xcopy *dot.go.tmpl ..\s\    /I /E /V /C /H /R /K /O /X /B /Y 
-xcopy *dot.go.tmpl ..\ss\   /I /E /V /C /H /R /K /O /X /B /Y 
+@If "%1"=="" goto iter
 
-xcopy *dot.go.tmpl ..\ssss\ /I /E /V /C /H /R /K /O /X /B /Y 
-xcopy *dot.go.tmpl ..\l\    /I /E /V /C /H /R /K /O /X /B /Y 
-xcopy *dot.go.tmpl ..\sl\   /I /E /V /C /H /R /K /O /X /B /Y 
-xcopy *dot.go.tmpl ..\xs\   /I /E /V /C /H /R /K /O /X /B /Y 
-xcopy *dot.go.tmpl ..\xl\   /I /E /V /C /H /R /K /O /X /B /Y 
+@Set args= /I /E /V /C /H /R /K /O /X /B /Y
+
+@xcopy %1	..\s\		%args%
+@xcopy %1	..\ss\		%args%
+
+@xcopy %1	..\ssss\	%args%
+@xcopy %1	..\l\		%args%
+@xcopy %1	..\sl\		%args%
+@xcopy %1	..\xs\		%args%
+@xcopy %1	..\xl\		%args%
+
+@Del 		..\s\Send{{.}}Proxy.dot.go.tmpl
+@Del 		..\l\Send{{.}}Proxy.dot.go.tmpl
+@Del 		..\sl\Send{{.}}Proxy.dot.go.tmpl
+@Del 		..\xs\Send{{.}}Proxy.dot.go.tmpl
+@Del 		..\xl\Send{{.}}Proxy.dot.go.tmpl
+
+@goto done
+
+:iter
+
+xcopy Core.nonil	basic\type\core.tmpl		/Y
+
+xcopy Core.tmpl		basic\type\IsUnsafe\core.tmpl	/Y
+
+xcopy Core.merge	basic\type\IsFloat\core.tmpl	/Y
+xcopy Core.merge	basic\type\IsInteger\core.tmpl	/Y
+xcopy Core.merge	basic\type\IsOrdered\core.tmpl	/Y
+xcopy Core.merge	basic\type\IsUnsigned\core.tmpl	/Y
+
+xcopy Core.basicmerge	basic\type\core.tmpl
+
+@Call %0 *core.tmpl
+@Call %0 *dot.go.tmpl
+
+@goto done
+
+:done
